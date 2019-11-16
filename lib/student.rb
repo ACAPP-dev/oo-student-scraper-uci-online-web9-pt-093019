db4d35357c76b290b46a00b6d3e64c0fc851598e
Andrew Capp <acapp909@gmail.com>
2019-11-16 17:28:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,12 +18,13 @@ class Student
 
   def add_student_attributes(attributes_hash)
     #binding.pry
-    twitter = attributes_hash[:twitter]
-    linkedin = attributes_hash[:linkedin]
-    github = attributes_hash[:github]
-    blog = attributes_hash[:blog]
-    profile_quote = attributes_hash[:profile_quote]
-    bio = attributes_hash[:bio]
+    self.twitter = attributes_hash[:twitter]
+    self.linkedin = attributes_hash[:linkedin]
+    self.github = attributes_hash[:github]
+    self.blog = attributes_hash[:blog]
+    self.profile_quote = attributes_hash[:profile_quote]
+    self.bio = attributes_hash[:bio]
+    
   end
 
   def self.all
